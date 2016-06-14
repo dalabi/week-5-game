6fbece93a49bfdd5383409731af72a68c474bc14
Damola <d.labi@yahoo.com>
2016-06-14 18:27:33 -0400

    sigh

---
+++ TriviaGame/assets/javascript/app.js
@@ -0,0 +1,11 @@
+document.ready(function(){
+	var correctAnswer = 0;
+	var incorrectAnswer = 0;
+	var unanswered = 0;
+
+	$('.firstSet').on('click', function checkAnswer() {
+		if (true) {}
+	})
+
+	
+});
